f048b133ba29cf6185e6c0e512cb45a30af5db22
Ibrahim-007-cloud <2504521@students.au.edu.pk>
2026-02-26 23:50:03 +0500

    Delete LAB 1 (2504521) directory

--- LAB 1 (2504521)/utilis.js
+++
@@ -1,7 +0,0 @@
-export const ayeshaprofile= {
-    name: "ayesha khan",
-    age: 28,
-    city : "lahore"
-};
-export const calculatearea=(lenght, widht)=>lenght*widht;
-export const greet =(name)=> `hello,${name}! Welcome back.`;
